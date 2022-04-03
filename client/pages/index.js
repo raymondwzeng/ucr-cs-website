f949a7803807eb535ac46a33f2360339ec1ca1c0
raymondwzeng <raymondwzeng@github.com>
2022-04-02 20:06:45 -0700

    Corrected name to UCR CompSci

--- client/pages/index.js
+++ client/pages/index.js
@@ -6,51 +6,85 @@ export default function Home() {
     return (
         <div className='container'>
         <Head>
-            <title>UCR CS Discord</title>
+            <title>UCR CompSci Discord</title>
         </Head>
         <Header/>
         <main>
-            <div className='biggest-text bolder'>Welcome to CS @ UCR!</div>
-            <div className='flex'>
-                <div id="intro" className='bigger-text'>
-                    CS @ UCR is a student/alumni-ran Discord community for CS, CEN, or EE majors, as well
-                    as anyone interested in those topics studying at the University of California - Riverside.
+            <div id='cover-all'>
+                <div id='vignette' className='flex'>
+                    
                 </div>
-            </div>
-            {/* TODO: Replace image with a non-norm one. */}
-            <img src="https://pbs.twimg.com/profile_images/1111520908/Normthenavel_400x400.png"/>
-            <div>
-                <a className='outline-button bigger-text button-main' target="_blank" rel='noopener norefferrer' href='https://discord.gg/S7MGSV4'>Click to join the server!</a>
+                <div id='center-text'>
+                    <div className='flex vertical-flex center-vertical center-horizontal tall'>
+                        <div className='extra-large-text bolder small-margin'>Welcome to UCR CompSci!</div>
+                        <div className='flex vertical-flex center-vertical'>
+                            <div id="intro" className='biggest-text'>
+                            UCR CompSci is a student/alumni-ran Discord community for CS, CEN, or EE majors, as well
+                            as anyone interested in those topics studying at the University of California - Riverside.
+                            </div>
+                            <a className='outline-button biggest-text button-main' target="_blank" rel='noopener noreferrer' href='https://discord.gg/S7MGSV4'>Click to join the server!</a>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div className='flex vertical-flex center-vertical vertical-gap-small' id="norm-navel-section">
             <h3>About Norm Navel Bot</h3>
                 <div className='bigger-text'>
-                    Norm Navel bot is our custom solution to role assignment and server management on the CS @ UCR Discord server, and 2-time winner of "creepiest mascot of the UC system".
+                    Norm Navel bot is our custom solution to role assignment and server management on the UCR CompSci Discord server, and 2-time winner of "creepiest mascot of the UC system".
                 </div>
             <div className='flex' id="norm-navel-info">
                     <a className='outline-button button-main' href="/documentation">View Command List</a>
-                    <a className='outline-button button-secondary' target="_blank" rel='noopener norefferer' href='https://github.com/raymondwzeng/norm-navel-bot'>View Source</a>
+                    <a className='outline-button button-secondary' target="_blank" rel='noopener noreferer' href='https://github.com/raymondwzeng/norm-navel-bot'>View Source</a>
             </div>
             </div>
         </main>
-
 
         <Footer/>
 
         <style jsx>
             {`
+                main {
+                    width: 100%;
+                }
+
+                .small-margin {
+                    margin-bottom: 1rem;
+                }
+
                 .flex {
                     justify-content: center;
-                    margin: 0.5rem 0;
+                    margin: 0 0;
                 }
 
                 .flex > div {
                     width: 80%;
                 }
 
+                #vignette {
+                    margin: 0;
+                    width: 100%;
+                    opacity: 0.3;
+                    background: linear-gradient(#000000, #5e5e5e);
+                }
+
+                #center-text {
+                    width: 100%;
+                    position: absolute;
+                    margin-top: 10rem;
+                    color: #ffffff;
+                }
+
+                #cover-all {
+                    display: flex;
+                    flex-grow: 10;
+                    background-image: url("./Splash.png");
+                    width: 100%;
+                    overflow: hidden;
+                }
+
                 #norm-navel-section {
-                    background-color: #C1C6C8;
-                    border-radius: 10px;
+                    width: 100%;
+                    background-color: #ffffff;
                 }
 
                 #norm-navel-info {
